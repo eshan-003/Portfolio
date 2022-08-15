20a3822ee2d9f951bb77d3c1ee145cdb8d99dd65
eshan-003 <107420267+eshan-003@users.noreply.github.com>
2022-08-15 17:03:53 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="-mt-20 rounded-2xl mx-auto w-2/3 md:w-full"
           />
         </div>
       </div>
